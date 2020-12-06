a2dd021d0ea4a90b9d411ac6c24be30dff8c8638
shyhhan tan <shyhhan@gmail.com>
2020-12-06 15:32:27 -0500

    Added tests

--- cachematrix.R
+++ cachematrix.R
@@ -38,13 +38,9 @@ cacheSolve <- function(x, ...) {
         inv
 }
 
-
+## Tests
 my_matrix <- makeCacheMatrix(matrix(1:4,2,2))
 my_matrix$get()
-
+my_matrix$getInverse()
 
 cacheSolve(my_matrix)
-
-my_matrix$getInverse()
-my_matrix$get()
-
